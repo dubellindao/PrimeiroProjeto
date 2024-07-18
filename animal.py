ec824f5ab88ee4c8d7775ebc7571cc0c99fe051a
EMANUEL ARTHUR DE SOUZA BRITO <20211094010042@ifrn.local>
2024-07-18 08:23:52 -0300

    subindo arquivo completo e objetos

--- animal.py
+++ animal.py
@@ -8,3 +8,21 @@ class Animal:
 
     def emitir_som(self):
         return "O animal emite um som."
+    
+class Cachorro(Animal):
+    def __init__(self, nome, idade, raca, peso):
+        super().__init__(nome, idade)
+        self.raca = raca
+        self.peso = peso
+
+    def exibir_detalhes(self):
+        return f"Nome do cachorro: {self._nome}, Idade: {self.idade} anos, Raça: {self.raca}, Peso: {self.peso} kg"
+
+    def emitir_som(self):
+        return "O cachorro late."
+
+    def calcular_idade_humana(self):
+        return self.idade * 7
+
+    def atualizar_peso(self, novo_peso):
+        self.peso = novo_peso
